0190fce8544d6884698aabfd9fa2bdb8857070cd
Markus Ast <m@rkusa.st>
2017-04-09 20:37:04 +0200

    use Rc<RefCell<>> instead of Arc<Mutex<>>
    since asynchrony (futures) does not require multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -6,54 +6,49 @@ use std::fmt;
 use std::error;
 use std::error::Error as _Error;
 use std::collections::VecDeque;
-use std::sync::{Arc, Mutex, LockResult, MutexGuard};
+use std::rc::Rc;
+use std::cell::RefCell;
 use futures::task::{Task, park};
 use futures::{Future, Poll, Async, BoxFuture};
-use tokio_core::reactor::Remote;
+use tokio_core::reactor::Handle;
 use tokio_postgres::error::ConnectError;
 use tokio_postgres::{Connection, TlsMode};
 use tokio_postgres::params::{ConnectParams, IntoConnectParams};
 
 pub struct InnerPool {
     params: ConnectParams,
-    remote: Remote,
+    handle: Handle,
     queue: VecDeque<Task>,
     conns: VecDeque<Connection>,
     size: i32,
 }
 
-pub struct Pool(Arc<Mutex<InnerPool>>);
+pub struct Pool(Rc<RefCell<InnerPool>>);
 
 enum Conn {
     Now(Connection),
     Future(BoxFuture<Connection, ConnectError>),
-    Err(Error),
     None,
 }
 
 impl Pool {
-    pub fn new<T>(params: T, remote: Remote, size: i32) -> Result<Self, ConnectError>
+    pub fn new<T>(params: T, handle: Handle, size: i32) -> Result<Self, ConnectError>
         where T: IntoConnectParams
     {
         let params = params.into_connect_params()
             .map_err(ConnectError::ConnectParams)?;
 
-        Ok(Pool(Arc::new(Mutex::new(InnerPool{
+        Ok(Pool(Rc::new(RefCell::new(InnerPool{
             params: params,
-            remote: remote,
+            handle: handle,
             queue: VecDeque::new(),
             conns: VecDeque::new(),
             size: size,
         }))))
     }
 
-    #[inline]
-    fn lock(&self) -> LockResult<MutexGuard<InnerPool>> {
-        self.0.lock()
-    }
-
     fn acquire(&self) -> Conn {
-        let mut inner = self.lock().unwrap();
+        let mut inner = self.0.borrow_mut();
         if inner.size == 0 {
             return Conn::None;
         }
@@ -63,18 +58,14 @@ impl Pool {
         match inner.conns.pop_front() {
             Some(conn) => Conn::Now(conn),
             None => {
-                if let Some(handle) = inner.remote.handle() {
-                    let conn = Connection::connect(inner.params.clone(), TlsMode::None, &handle);
-                    Conn::Future(conn)
-                } else {
-                    Conn::Err(Error::EventLoop)
-                }
+                let conn = Connection::connect(inner.params.clone(), TlsMode::None, &inner.handle);
+                Conn::Future(conn)
             }
         }
     }
 
     fn release(&self, conn: Connection) {
-        let mut inner = self.lock().unwrap();
+        let mut inner = self.0.borrow_mut();
         inner.conns.push_back(conn);
         inner.size += 1;
 
@@ -84,20 +75,20 @@ impl Pool {
     }
 
     fn connection_lost(&self) {
-        let mut inner = self.lock().unwrap();
+        let mut inner = self.0.borrow_mut();
         inner.size += 1;
     }
 
     fn enqueue(&self, task: Task) {
-        let mut inner = self.lock().unwrap();
+        let mut inner = self.0.borrow_mut();
         inner.queue.push_back(task);
     }
 
-    pub fn with_connection<F, R, I, E>(&self, f: F) -> Box<Future<Item=I, Error=E> + Send + 'static>
-        where F: FnOnce(Connection) -> R + Send + 'static,
-              R: Future<Item = (I, Connection), Error = (E, Option<Connection>)> + Send + 'static,
-              I: Send + 'static,
-              E: From<Error> + Send + 'static
+    pub fn with_connection<F, R, I, E>(&self, f: F) -> Box<Future<Item=I, Error=E> + 'static>
+        where F: FnOnce(Connection) -> R + 'static,
+              R: Future<Item = (I, Connection), Error = (E, Option<Connection>)> + 'static,
+              I: 'static,
+              E: From<Error> + 'static
     {
         let pool1 = self.clone();
         let pool2 = self.clone();
@@ -105,11 +96,11 @@ impl Pool {
             pool: self.clone(),
             conn: None,
         };
-        conn.map_err(move |err| {
-                let mut inner = pool1.lock().unwrap();
+        let fut = conn.map_err(move |err| {
+                let mut inner = pool1.0.borrow_mut();
                 inner.size += 1;
 
-                E::from(err)
+                E::from(Error::Connect(err))
             })
             .and_then(move |conn| {
                 f(conn).then(move |res| {
@@ -128,8 +119,8 @@ impl Pool {
                         }
                     }
                 })
-            })
-            .boxed()
+            });
+        Box::new(fut)
     }
 }
 
@@ -146,23 +137,22 @@ struct FutureConnection {
 
 impl Future for FutureConnection {
     type Item = Connection;
-    type Error = Error;
+    type Error = ConnectError;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         if let Some(ref mut conn) = self.conn {
-            return conn.poll().map_err(Error::Connect);
+            return conn.poll();
         }
 
         match self.pool.acquire() {
             Conn::Now(conn) => Ok(Async::Ready(conn)),
             Conn::Future(mut conn) => {
-                let result = conn.poll().map_err(Error::Connect);
+                let result = conn.poll();
                 if let Ok(Async::NotReady) = result {
                     self.conn = Some(conn);
                 }
                 result
             }
-            Conn::Err(err) => Err(err),
             Conn::None => {
                 self.pool.enqueue(park());
                 Ok(Async::NotReady)
@@ -174,7 +164,6 @@ impl Future for FutureConnection {
 #[derive(Debug)]
 pub enum Error {
     Connect(ConnectError),
-    EventLoop,
 }
 
 impl fmt::Display for Error {
@@ -187,14 +176,12 @@ impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::Connect(ref err) => err.description(),
-            Error::EventLoop => "Event loop not running",
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             Error::Connect(ref err) => Some(err as &error::Error),
-            _ => None,
         }
     }
 }
